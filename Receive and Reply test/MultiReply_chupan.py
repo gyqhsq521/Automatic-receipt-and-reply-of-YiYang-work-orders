dacde411439634e4bc1610c327c9118d849e3860
gyqhsq521 <81281046+gyqhsq521@users.noreply.github.com>
2024-05-03 21:16:05 +0800

    Add files via upload

--- Receive and Reply test/MultiReply_chupan.py
+++ Receive and Reply test/MultiReply_chupan.py
@@ -392,7 +392,7 @@ def reply_gongdan(username,password,frequence,district):
                 #确认回单前当前时间
                 c_time = datetime.now().replace(microsecond=0)
 
-                if data_list_name[i-1] != '' and data_list_personnel[h-1] != '总部自动派单' and data_list_personnel[h-1] != '省分自动派单':                     
+                if data_list_name[i-1] != '' and data_list_personnel[i-1] != '总部自动派单' and data_list_personnel[i-1] != '省分自动派单':                     
 
                     #回复超过派单时间2个小时的工单
                     if (c_time - data_list_time[i-1]).total_seconds() >= 7200.0:
